e4e71fe62d36f08c7af56082a5f4b470cce8d18d
Munashe <munashedov32@gmail.com>
2022-05-10 16:51:25 -0700

    adds ps script to auto compile standalone html

---
+++ produce-standalone-html.ps1
@@ -0,0 +1,40 @@
+﻿$html = Get-Content -Path $PSScriptRoot\dist\index.html
+$js = Get-Content -Path $PSScriptRoot\dist\main.js
+$css = Get-Content -Path $PSScriptRoot\dist\style.css
+[string[]]$output
+
+$outputDir = "$PSScriptRoot\dist\"
+
+$fileNameIndex = $html.IndexOf("<!--filename-->")+1;
+$outputFileName = $html[$fileNameIndex].Split(" ")[1].Split("=")[1];;
+
+$html[$fileNameIndex] = "";
+$html[($fileNameIndex-1)] = "";
+
+$cssTarget = '<link rel="stylesheet" href="./style.css"></style>'
+$jsTarget = '<script src="./main.js"></script>'
+
+$cssIdx = $html.IndexOf($cssTarget);
+$jsIdx = $html.IndexOf($jsTarget);
+
+
+$output += $html[0..($cssIdx-1)];
+
+$output += "<style>";
+$output += $css;
+$output += "</style>";
+
+
+$output += $html[($cssIdx+1)..($jsIdx-1)];
+
+$output += "<script>";
+$output += $js;
+$output += "</script>";
+$output[$output.IndexOf("//# sourceMappingURL=main.js.map")] = "";
+
+
+$output += $html[($jsIdx+1), $html.Length];
+
+$output | Out-File -FilePath ($outputDir + $outputFileName)
+
+Remove-Variable * -ErrorAction SilentlyContinue
