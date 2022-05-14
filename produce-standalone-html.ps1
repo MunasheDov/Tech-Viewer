38f8b1bfb57edbc55b303f841de14c6571b96c66
Munashe <munashedov32@gmail.com>
2022-05-13 19:31:14 -0700

    single html contains base64 assets

--- produce-standalone-html.ps1
+++ produce-standalone-html.ps1
@@ -5,14 +5,24 @@ $css = Get-Content -Path $PSScriptRoot\dist\style.css
 
 $outputDir = "$PSScriptRoot\dist\"
 
-$fileNameIndex = $html.IndexOf("<!--filename-->")+1;
-$outputFileName = $html[$fileNameIndex].Split(" ")[1].Split("=")[1];;
-
-$html[$fileNameIndex] = "";
-$html[($fileNameIndex-1)] = "";
-
 $cssTarget = '<link rel="stylesheet" href="./style.css"></style>'
 $jsTarget = '<script src="./main.js"></script>'
+
+
+$fileNameTag = "<!-- filename="
+
+$searchResult = $html | Select-String -Pattern $fileNameTag | Select-Object -Property LineNumber, Line
+
+if ($searchResult) {
+    $fileNameIndex = ($searchResult.LineNumber-1);
+    $outputFileName = $html[$fileNameIndex].Split('"')[1];
+    $html[$fileNameIndex] = "";
+} else {
+    "Failed to find '$fileNameTag' in index.html";
+    "Add fileNameTag in the form of: $fileNameTag`"[filename]`" -->";
+    return;
+}
+
 
 $cssIdx = $html.IndexOf($cssTarget);
 $jsIdx = $html.IndexOf($jsTarget);
@@ -27,14 +37,19 @@ $output += "</style>";
 
 $output += $html[($cssIdx+1)..($jsIdx-1)];
 
+
 $output += "<script>";
 $output += $js;
 $output += "</script>";
+
 $output[$output.IndexOf("//# sourceMappingURL=main.js.map")] = "";
 
 
 $output += $html[($jsIdx+1), $html.Length];
 
+
 $output | Out-File -FilePath ($outputDir + $outputFileName)
+
+"Successfully compiled and saved '$outputFileName'"
 
 Remove-Variable * -ErrorAction SilentlyContinue
